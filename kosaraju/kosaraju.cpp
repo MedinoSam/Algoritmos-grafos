b60d34f0ba9e5cae8a74b27a991c7bfda21e4165
MedinoSam <samuelejosuetutorial@gmail.com>
2025-05-17 17:20:24 -0300

    Kosaraju finalizado

--- kosaraju/kosaraju.cpp
+++ kosaraju/kosaraju.cpp
@@ -1,6 +1,8 @@
 #include <vector>
 #include <iostream>
 #include <stack>
+#include <string.h>
+#include <fstream>
 using namespace std;
 
 void dfsComPilhaPrioridade(vector<vector<int>> grafo, stack<int>&pilhaPrioridade, vector<bool>&visitado, int v){
@@ -53,10 +55,38 @@ void kosarajuAlgoritmo(vector<vector<int>> grafo, vector<vector<int>> grafoTrans
 }
 
 
-int main(){
+int main(int argc, char **argv){
+    
+    string input_file = "";
+    string output_file = "";
+    //percorre a string do terminal
+    for(int i=1; i<argc; i++){
+        if(strcmp(argv[i], "-h") == 0){
+            cout << "Help:" << endl;
+            cout << "-h: mostra o help" << endl;
+            cout << "-o <arquivo>: redireciona a saida para o arquivo" << endl;
+            cout << "-f <arquivo>: indica o 'arquivo' que contém o grafo de entrada" << endl;
+            return 0;
+        }
+        else if(strcmp(argv[i], "-o") == 0 && i < argc - 1) output_file = argv[++i];
+
+        else if(strcmp(argv[i], "-f") == 0 && i < argc - 1) input_file = argv[++i];
+    }
+
+    if(input_file == ""){
+        cerr << "Sem input especificado. Use o parametro -f" << endl;
+        return 1;
+    }
+
+    ifstream fin(input_file);
+
+    if(!fin){
+        cerr << "Nao foi possivel abrir o arquivo de entrada: " << input_file << endl;
+        return 1;
+    }
 
     int  n, m;
-    cin >> n >> m;
+    fin >> n >> m;
 
     vector<vector<int>> grafo(n+1);
     vector<vector<int>> grafoTransposto(n+1);
@@ -65,18 +95,39 @@ int main(){
     int v,w;
     for (int i=1; i<=m; i++){
 
-        cin >> v >> w;
+        fin >> v >> w;
         grafo[v].push_back(w);
         grafoTransposto[w].push_back(v);
     }
-    
+
+    fin.close();
+
     kosarajuAlgoritmo(grafo, grafoTransposto, componentesFortementesConexas);
 
-    for (int i = 0; i < componentesFortementesConexas.size(); i++) {
-        cout << "Componente Fortemente Conexa " << i+1 << ": ";
-        for (int v : componentesFortementesConexas[i]) {
-        cout << v << " ";
+     if(!output_file.empty()){
+
+        ofstream fout(output_file);
+        if(!fout){
+            cerr << "Nao foi possivel abrir o arquivo de saida: " << output_file << endl;
+            return 1;
         }
-        cout << endl;
+
+        for (int i = 0; i < componentesFortementesConexas.size(); i++) {
+            fout << "Componente Fortemente Conexa " << i+1 << ": ";
+                for (int v : componentesFortementesConexas[i]) {
+                    fout << v << " ";
+                }
+            fout << endl;
+        }
+        fout.close();
     }
+    else{
+        for (int i = 0; i < componentesFortementesConexas.size(); i++) {
+                for (int v : componentesFortementesConexas[i]) {
+                    cout << v << " ";
+                }
+        cout << endl;
+        }
+    }
+    return 0;
 }
