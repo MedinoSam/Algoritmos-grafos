6569c440afda347f7aa166a7fd0d8918377e7ba8
MedinoSam <samuelejosuetutorial@gmail.com>
2025-05-17 18:25:24 -0300

    Prim finalizado

--- prim/prim.cpp
+++ prim/prim.cpp
@@ -2,23 +2,61 @@
 #include <vector>
 #include <queue>
 #include <limits>
+#include <string.h>
+#include <fstream>
 using namespace std;
 
-int main(){
+int main(int argc, char **argv){
+    string input_file = "";
+    string output_file = "";
+    int verticeInicial = 1;
+    bool solucao = false;
+
+    for(int i=1; i<argc; i++){
+        if(strcmp(argv[i], "-h") == 0){
+            cout << "Help:" << endl;
+            cout << "-h: mostra o help" << endl;
+            cout << "-o <arquivo>: redireciona a saida para o arquivo" << endl;
+            cout << "-f <arquivo>: indica o 'arquivo' que contém o grafo de entrada" << endl;
+            cout << "-s: mostra a solução" << endl;
+            cout << "-i: vertice  inicial" << endl;
+            return 0;
+        }
+        else if(strcmp(argv[i], "-o") == 0 && i < argc - 1) output_file = argv[++i];
+
+        else if(strcmp(argv[i], "-f") == 0 && i < argc - 1) input_file = argv[++i];
+
+        else if(strcmp(argv[i], "-i") == 0 && i < argc - 1) verticeInicial = atoi(argv[++i]);
+
+        else if(strcmp(argv[i], "-s") == 0) solucao = true;
+    }
+
+    if(input_file == ""){
+        cerr << "Sem input especificado. Use o parametro -f" << endl;
+        return 1;
+    }
+
+    ifstream fin(input_file);
+
+    if(!fin){
+        cerr << "Nao foi possivel abrir o arquivo de entrada: " << input_file << endl;
+        return 1;
+    }
 
     int n, m;
 
-    cin >> n >> m;
+    fin >> n >> m;
 
     vector<vector<pair<int, int>>> grafo(n+1);
 
     for (int i=1; i<=m; i++){
         int u, v, peso;
-        cin >> u >> v >> peso;
+        fin >> u >> v >> peso;
 
         grafo[u].emplace_back(v, peso);
         grafo[v].emplace_back(u, peso);
     }
+    fin.close();
 
     vector<bool> visitado(n+1, false);
     vector<int> chave(n+1, numeric_limits<int>::max());
@@ -48,12 +86,44 @@ int main(){
         } 
     }
 
-    cout << "custo total: " << custoTotal << endl;
+    if(!output_file.empty()){
+        ofstream fout(output_file);
+        if(!fout){
+            cerr << "Nao foi possivel abrir o arquivo de saida: " << output_file << endl;
+            return 1;
+        }
 
-    cout << "Arestas da  AGM: " << endl;
-    for(int v = 1; v <= n; v++){
-        if(pai[v] != -1){
-            cout << pai[v]  << " - " << v << " (peso " << chave[v] << ")" << endl;
+        if(solucao){
+            for(int v = 1; v <= n; v++){
+                if(pai[v] != -1){
+                    fout << "(" << pai[v] << "," << v << ")";
+                    if(v!=n)
+                        fout << " ";
+                    else
+                        fout << endl;
+                }
+            }  
+        }
+        else{
+            fout << custoTotal << endl;
+        }
+        fout.close();
+    }
+    else{
+        if(solucao){
+            for(int v = 1; v <= n; v++){
+                if(pai[v] != -1){
+                    cout << "(" << pai[v] << "," << v << ")";
+                    if(v!=n)
+                        cout << " ";
+                    else
+                        cout << endl;
+                }
+            }  
+        }
+        else{
+            cout << custoTotal << endl;
         }
     }
+    return 0;
 }
